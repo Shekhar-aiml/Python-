2289b9ccc8bdc2b619fc4e342c763719eb6820ba
Shekhar <244034301+Shekhar-U@users.noreply.github.com>
2026-05-19 15:32:17 +0530

    first commit

---
+++ syntax.py
@@ -0,0 +1,18 @@
+"""
+Python Syntax
+Syntax means the rules for writing code correctly in Python (like grammar in English).
+
+In Python (programming language),
+ syntax tells you how to structure statements so the computer understands them.
+
+"""
+#1 Printing Output 
+
+print('Hello World!!')
+
+#2Basic rule: indentation matters
+
+#Python uses spaces to define blocks:
+
+if True:
+    print("Hello")
