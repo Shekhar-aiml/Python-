323078bc1f18b6f722bf65d67188ca32d4a1ac94
Shekhar <244034301+Shekhar-U@users.noreply.github.com>
2026-05-19 16:13:01 +0530

    commit

---
+++ syntax.py
@@ -0,0 +1,59 @@
+"""
+Python Syntax
+Syntax means the rules for writing code correctly in Python (like grammar in English).
+
+In Python (programming language),
+ syntax tells you how to structure statements so the computer understands them.
+
+"""
+#1 Printing Output 
+
+print('Hello World!!')
+
+#2 Basic rule: indentation matters
+
+# Python uses spaces to define blocks:
+
+if True:
+    print("Hello")
+
+#3 Statements (instructions)
+
+#Each line is usually one instruction:
+
+x = 10
+print(x)
+
+#4 Variables (no declaration needed)
+name = "Asha"
+age = 20
+
+#5 Keywords (special words)
+"""
+Python uses reserved words:
+
+if, else, elif
+for, while
+def, class 
+True, False, None 
+"""
+
+#6 Conditions
+if age > 18:
+    print("Adult")
+
+#7 Loops
+for i in range(3):
+    print(i)
+
+#8 Functions
+def greet():
+    print("Hello")
+
+
+"""
+ General syntax rules
+No semicolons needed (; optional but not used normally)
+Case-sensitive (Name ≠ name)
+Uses : to start blocks
+Uses indentation instead of { } """
