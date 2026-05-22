8838e3920015d8b36300c704b782c1774d7e0052
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-05-23 01:00:07 +0530

    Add basic operator demonstrations in Python

--- Fundamentals/7.operators.py
+++ Fundamentals/7.operators.py
@@ -1,1 +1,72 @@
+print('Enter two numbers to perform operation:\n')
 
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+
+#Arithmatic Operators
+
+print('\nArithmetic Operators: ')
+
+print('Sum: ',a + b)
+print('Difference: ',a - b)
+print('Product : ',a * b)
+print('Division: ',a / b)
+print('Floor Division: ',a // b)
+print('Modulus: ',a % b)
+print('Exponentiation: ',a ** b)
+
+#Relational Operators
+
+print('\nRelational Operators')
+
+print('a > b: ',a > b)
+print('a < b: ',a < b)
+print('a >= b: ',a >= b)
+print('a <= b:',a <= b)
+print('a == b: ',a == b)
+print('a != b: ',a != b)
+
+#logical opertors 
+
+print('\nLogical Operator: ')
+
+
+print('a and b: ',a and b)
+print('a or b: ',a or b)
+print('not a: ',not a)
+
+#Bitwise Operators
+
+print('\nBitwise Operators')
+print('a & b: ',a & b )
+print('a | b: ',a | b)
+print('a ^ b: ',a ^ b)
+print('~a: ',~a)
+print('a >> b: ',a >> b)
+print('a << b: ',a << b)
+      
+print('\nAssignment Operators: ')
+
+# x is used to keep the original value of a unchanged
+
+x = a
+x += b
+print('x+=b: ',x)
+x-=b
+print('x-=b: ',x)
+x*=b
+print('x*=b: ',x)
+x/=b
+print('x/=b: ',x)
+x//=b
+print('x//=b: ',x)
+x**=b
+print('x**=b: ',x)
+
+
+#Membership Operator
+
+#in / not in
+print('\nMembership Operator: ')
+print('D' in 'Delhi')
+print('D' not in 'Delhi')
