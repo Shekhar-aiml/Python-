faa97953c971b8b6c0cef68ac5a7439f40ef838e
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-05-24 21:11:48 +0530

    Update and rename while-loop to 10.while-loop.py

---
+++ Fundamentals/10.while-loop.py
@@ -0,0 +1,4 @@
+#While Loop
+
+"""While loop repeatedly executes a block of code as long as the given condition remains true. 
+When the condition becomes false, the line immediately after the loop in the program is executed."""
