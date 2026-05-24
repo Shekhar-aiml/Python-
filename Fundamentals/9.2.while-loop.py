96ebf0800d3f44f8c748bdbdc33251fe2dcca782
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-05-24 21:25:22 +0530

    Update 9.2.while-loop.py

--- Fundamentals/9.2.while-loop.py
+++ Fundamentals/9.2.while-loop.py
@@ -2,3 +2,13 @@
 
 """While loop repeatedly executes a block of code as long as the given condition remains true. 
 When the condition becomes false, the line immediately after the loop in the program is executed."""
+
+
+
+#Table
+number = int(input('Enter a number: '))
+
+i = 1
+while i<11:
+    print(number,'*',i,'=',number*i)
+    i +=1
