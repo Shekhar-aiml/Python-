85f8f6bbd3312eac6650077bea6a79e757483f9b
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-05-27 14:34:38 +0530

    Update for-loop.py

--- Fundamentals/for-loop.py
+++ Fundamentals/for-loop.py
@@ -17,9 +17,23 @@ curr_pop = 10000
 for i in range(10,0,-1):
     print(i,curr_pop)
 
-    curr_pop = curr_pop - 0.1* curr_pop
+    curr_pop = curr_pop / 1.1
 
+# Sequence sum
+# 1/1! + 2/2! + 3/3! + ...'
 
+n = int(input('Enter n: '))
+
+result = 0
+fact = 1
+
+for i in range (1,n + 1): 
+    
+    fact = fact * i
+    result = result + i / fact
+    
+
+print(result)
 
 
 
