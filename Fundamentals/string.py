888797f02b538a40455504f41f567b2d1ca88508
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-05-28 20:09:21 +0530

    Update and rename string.py to string.py

---
+++ Fundamentals/string.py
@@ -0,0 +1,18 @@
+#Python String
+
+""" Strings are sequence of characters written inside quotes. It can include letters, numbers, symbols and spaces.
+Python does not have a separate character type.
+
+A single character is treated as a string of length one.
+Strings are commonly used for text handling and manipulation.
+Creating a String
+Strings can be created using either single ('...') or double ("...") quotes. Both behave the same.
+"""
+
+#Example 
+s = 'Hello World'
+s = "It's raining outside"
+
+s = '''Multiline String'''
+s = """Multiline String """
+
