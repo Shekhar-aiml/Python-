818a0342b4793e3b924717c9ec9762ddfae1efd1
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-05-28 12:58:33 +0530

    Update and rename nested-loop,py to nested-loop.py

---
+++ Fundamentals/nested-loop.py
@@ -0,0 +1,5 @@
+""" Python Nested Loops
+
+In Python, there are two types of loops: for loop and while loop. Using these loops,
+we can create nested loops, which means loops inside a loop. 
+For example, a while loop inside a for loop, or a for loop inside another for loop."""
