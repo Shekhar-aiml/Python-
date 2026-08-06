9d293b3a64fc611d5e772a77532cf48deda5ef8a
Shekhar <244034301+Shekhar-aiml@users.noreply.github.com>
2026-08-06 19:59:19 +0530

    Update 01_intoduction.py

--- Lists/01_intoduction.py
+++ Lists/01_intoduction.py
@@ -1,1 +1,23 @@
+"""
+Python Lists
+
+A list is a built-in Python data structure used to store
+an ordered collection of elements.
+
+Characteristics
+---------------
+ Ordered
+ Mutable
+ Dynamic
+ Allows duplicates
+ Heterogeneous
+"""
+
+numbers = [10, 20, 30]
+fruits = ["Apple", "Banana"]
+mixed = [1, "Python", 3.5, True]
+
+print(numbers)
+print(fruits)
+print(mixed)
 
